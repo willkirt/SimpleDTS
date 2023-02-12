3b92b2f0a7ae01c77fb2ccf9c189256f20217e2c
William Kirtlink <wmkirtlink@email.neit.edu>
2023-02-12 17:26:07 -0500

    basics in place

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
+const { response } = require("express");
+var port = process.env.port||3000;
+var db = require("./config/database");
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(__dirname+'/dts'));
+
+mongoose.connect(db.mongoURI,{
+    useNewURLParser:true
+}).then(function(){
+    console.log("connected to MongoDB Database");
+}).catch(function(err){
+    console.log(err);
+});
+
+// Main route
+app.get('/', function(req, res){
+    //res.send('No');
+    res.redirect('./index.html');
+});
+
+app.listen(port, function(){
+    console.log(`Running on port ${port}.`);
+})
